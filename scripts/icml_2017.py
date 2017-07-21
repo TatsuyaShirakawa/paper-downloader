c151cd6cd9e36cb88d1210cea43fffe5d0cb1c4a
Tatsuya Shirakawa <tatsuya@Tatsuyas-MacBook-Pro.local>
2017-07-21 18:12:28 +0900

    updaetd icml_2017.py

--- scripts/icml_2017.py
+++ scripts/icml_2017.py
@@ -5,43 +5,76 @@ import urllib
 import click
 import arxiv
 
-papers_url = 'https://2017.icml.cc/Conferences/2017/AcceptedPapers'
+papers_url = 'http://proceedings.mlr.press/v70/'
+
+def url_encode(url):
+    p = urllib.parse.urlparse(url)
+    url = '{}://{}{}'.format(p.scheme, p.netloc, '/'.join(urllib.parse.quote_plus(x) for x in p.path.split('/')))
+    return url
 
 @click.command()
 @click.argument('result_dir')
 def main(result_dir):
 
-    html = urllib.request.urlopen('https://2017.icml.cc/Conferences/2017/AcceptedPapers')
+    html = urllib.request.urlopen(papers_url)
     soup = BeautifulSoup(html, "html.parser")
 
-    titles = [x.text for x in soup.find_all('strong')]
-    print('{} papers found'.format(len(titles)))
+    papers = soup.find_all('div', attrs={'class': 'paper'})
+    print('{} papers found'.format(len(papers)))
 
     if not os.path.exists(result_dir):
         os.makedirs(result_dir)
 
-    num_founds = 0
-    num_not_founds = 0
-    for title in titles:
+    for paper in papers:
 
-        entry = arxiv.search(title)
-        if entry is not None:
-            print('found    :', title)
-            link = entry.find('link', attrs=dict(title='pdf'))
-            if link is None:
-                num_not_founds += 1
-                continue
-            paper_url = link['href']
-            response = urllib.request.urlopen(paper_url)
-            with open(os.path.join(result_dir, paper_url.split('/')[-1]), 'wb') as fout:
-                fout.write(response.read())
-            num_founds += 1
-        else:
-            print('not found:', title)
-            num_not_founds += 1
+        title = paper.find('p', attrs={'class': 'title'}).text
+        links = paper.find('p', attrs={'class': 'links'})
 
-    print('founds:', num_founds)
-    print('not founds:', num_not_founds)
+        print( title, '...' )
+
+        paper_dir = os.path.join(result_dir, title.replace(' ', '_').lower())
+        if not os.path.exists(paper_dir):
+            os.makedirs(paper_dir)
+
+        for a in links.find_all('a'):
+            if a.text == 'abs':
+                # abs
+                pass
+            elif a.text == 'Download PDF':
+                # paper pdf link
+                url = a['href']
+                print(url)
+                url = url_encode(url)
+                save_path = os.path.join(paper_dir, url.split('/')[-1])
+                if os.path.exists(save_path):
+                    continue
+                response = urllib.request.urlopen(url)
+                with open(save_path, 'wb') as fout:
+                    fout.write(response.read())
+            elif a.text == 'Supplementary PDF':
+                # paper supplementary pdf link
+                url = a['href']
+                print(url)
+                url = url_encode(url)
+                save_path = os.path.join(paper_dir, url.split('/')[-1])
+                if os.path.exists(save_path):
+                    continue
+                response = urllib.request.urlopen(url)
+                with open(save_path, 'wb') as fout:
+                    fout.write(response.read())
+            elif a.text == 'Supplementary ZIP':
+                # paper supplementary ZIP link
+                url = a['href']
+                print(url)
+                url = url_encode(url)
+                save_path = os.path.join(paper_dir, url.split('/')[-1])
+                if os.path.exists(save_path):
+                    continue
+                response = urllib.request.urlopen(url)
+                with open(save_path, 'wb') as fout:
+                    fout.write(response.read())
+            else:
+                print( 'unknown link', a.text )
 
 
 if __name__ == '__main__':
